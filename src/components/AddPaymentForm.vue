8cedfe6c9089503c8ddd491608ca1d03fc081a8e
st1ssssss <79720369+st1ssssss@users.noreply.github.com>
2021-11-22 11:34:59 +0500

    Les3
    Не совсем понял в каком виде нужно было реализовать компонент с кнопкой

---
+++ src/components/AddPaymentForm.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="wrapper">
+    <input class="input-add" placeholder="Payment amount" v-model="value" />
+    <input class="input-add" placeholder="Payment type" v-model="category" />
+    <input class="input-add" placeholder="Payment date" v-model="date" />
+    <button class="button-add" @click="onSaveClick">Save!</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: "",
+      category: "",
+      date: "",
+    };
+  },
+
+  computed: {
+    getCurrentDate() {
+      const today = new Date();
+      const d = today.getDate();
+      const m = today.getMonth() + 1;
+      const y = today.getFullYear();
+      return `${d}.${m}.${y}`;
+    },
+  },
+
+  methods: {
+    check(a) {
+      console.log(a);
+    },
+
+    onSaveClick() {
+      const data = {
+        value: +this.value,
+        category: this.category,
+        date: this.date || this.getCurrentDate,
+      };
+      this.$emit("addNewPayment", data);
+    },
+  },
+};
+</script>
+
+<style lang="sass">
+.wrapper
+  display: flex
+  flex-direction: column
+  width: 200px
+
+.input-add
+  border-radius: 3px
+  margin: 5px 0
+  padding: 5px 5px
+  transition: all 0.2s
+  outline: none
+  border: 1px solid #ccc
+.input-add:focus
+  border: 1px solid #000
+
+.button-add
+  width: 50%
+  padding: 5px
+  color: #fff
+  text-transform: uppercase
+  align-self: flex-end
+  background: #00ADAB
+  border: #00ADAB
+  border-radius: 3px
+</style>
---
+++ src/components/PaymentsDisplay.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="payment-block">
+    <table class="table">
+      <tr>
+        <th>#</th>
+        <th>Date</th>
+        <th>Category</th>
+        <th>Value</th>
+      </tr>
+      <tr v-for="(item, idx) in items" :key="idx">
+        <td>{{ idx + 1 }}</td>
+        <td>{{ item.date }}</td>
+        <td>{{ item.category }}</td>
+        <td>{{ item.value }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+  },
+};
+</script>
+
+<style lang='sass'>
+.payment-block
+  display: flex
+  flex-direction: column
+
+  .table
+    width: 600px
+    border-collapse: collapse
+
+.nav-bar
+  display: flex
+  justify-content: space-between
+
+td
+  font-family: Helvetica, sans-serif
+  text-align: left
+  border-top: 1px solid #ccc
+  padding: 10px 20px 10px 0
+
+th
+  font-family: Helvetica, sans-serif
+
+  text-align: left
+  border-bottom: 1px solid #ccc
+  padding: 10px 0
+</style>
