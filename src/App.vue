f59a5b49ca55417f51a5417d50e6682b95345a69
st1ssssss <79720369+st1ssssss@users.noreply.github.com>
2021-12-01 19:06:07 +0300

    Les5
    Не совсем разобрался как нужно было сделать query-строку, реализовал только внешний вид ее в url-строке. Интересно как нужно было ее оформить

---
+++ src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <header>
+      <router-link to="/dashboard">Dashboard</router-link>/
+
+      <router-link to="/about">About</router-link>/
+      <router-link to="/404">Not Found</router-link>
+    </header>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {};
+  },
+  created() {
+    //this.$router.push({ name: "Dashboard" });
+  },
+};
+</script>
+
+<style lang="sass">
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
+  margin-top: 60px
+.main-heading
+  font-family: Arial, Helvetica, sans-serif
+
+.nav-bar
+  display: flex
+
+.button
+  width: 150px
+  padding: 5px
+  color: #fff
+  text-transform: uppercase
+  align-self: flex-end
+  background: #00ADAB
+  border: #00ADAB
+  border-radius: 3px
+
+a
+  text-decoration: none
+</style>
---
+++ src/components/PaymentsDisplay.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="payment-block">
+    <h2 class="main-heading">My personal costs</h2>
+    {{ getFPV }}
+    <table class="table">
+      <tr>
+        <th>#</th>
+        <th>Date</th>
+        <th>Category</th>
+        <th>Value</th>
+      </tr>
+      <tr v-for="(item, idx) in items" :key="idx">
+        <td>{{ idx + 1 }}</td>
+        <td>{{ item.date }}</td>
+        <td>{{ item.category }}</td>
+        <td>{{ item.value }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => {
+        return [];
+      },
+    },
+  },
+  computed: {
+    getFPV() {
+      return this.$store.getters.getFullPaymentsValue;
+    },
+  },
+};
+</script>
+
+<style lang='sass'>
+.payment-block
+  display: flex
+  flex-direction: column
+
+  .table
+    width: 600px
+    border-collapse: collapse
+
+.nav-bar
+  display: flex
+  justify-content: space-between
+
+td
+  font-family: Helvetica, sans-serif
+  text-align: left
+  border-top: 1px solid #ccc
+  padding: 10px 20px 10px 0
+
+th
+  font-family: Helvetica, sans-serif
+
+  text-align: left
+  border-bottom: 1px solid #ccc
+  padding: 10px 0
+</style>
